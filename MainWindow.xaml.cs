31a39462078808178d19791331a49dc58dc40cc8
Liza Polozenko <lizapolozenko03@gmail.com>
2026-04-16 14:09:19 +0300

    task 8.2: bind commands to buttons and logic

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,14 +15,53 @@ using System.Windows.Shapes;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            CommandBinding saveCommand = new CommandBinding(ApplicationCommands.Save, execute_Save, canExecute_Save);
+            CommandBindings.Add(saveCommand);
+
+            CommandBinding openCommand = new CommandBinding(ApplicationCommands.Open, execute_Open, canExecute_Open);
+            CommandBindings.Add(openCommand);
+
+            CommandBinding clearCommand = new CommandBinding(ApplicationCommands.Delete, execute_Clear, canExecute_Clear);
+            CommandBindings.Add(clearCommand);
+        }
+
+        private void canExecute_Save(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (txtDocument.Text.Trim().Length > 0) e.CanExecute = true;
+            else e.CanExecute = false;
+        }
+
+        private void execute_Save(object sender, ExecutedRoutedEventArgs e)
+        {
+            System.IO.File.WriteAllText("d:\\myFile.txt", txtDocument.Text);
+            MessageBox.Show("The file was saved!");
+        }
+
+        private void canExecute_Open(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void execute_Open(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Тут міг би бути код відкриття файлу!");
+        }
+
+        private void canExecute_Clear(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (txtDocument.Text.Length > 0) e.CanExecute = true;
+            else e.CanExecute = false;
+        }
+
+        private void execute_Clear(object sender, ExecutedRoutedEventArgs e)
+        {
+            txtDocument.Clear();
         }
     }
 }
